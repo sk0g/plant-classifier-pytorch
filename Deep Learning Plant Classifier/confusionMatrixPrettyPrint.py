52bbb6c83ba18f7d43773c39b3f59c09ed8a6e3c
Ani Mehta <ani.mehta@mail.com>
2020-04-21 22:41:25 +1000

    Formatting fixes

--- Deep Learning Plant Classifier/confusionMatrixPrettyPrint.py
+++ Deep Learning Plant Classifier/confusionMatrixPrettyPrint.py
@@ -14,14 +14,15 @@ References:
 
 import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
-# imports
 import numpy as np
 import seaborn as sn
 from matplotlib.collections import QuadMesh
 
 
-def get_new_fig(fn, figsize=[9, 9]):
+def get_new_fig(fn, figsize=None):
     """ Init graphics """
+    if figsize is None:
+        figsize = [9, 9]
     fig1 = plt.figure(fn, figsize)
     ax1 = fig1.gca()  # Get Current Axis
     ax1.cla()  # clear existing plot
@@ -34,8 +35,8 @@ def configcell_text_and_colors(array_df, lin, col, oText, facecolors, posi, fz, 
       and return text elements to add and to dell
       @TODO: use fmt
     """
-    text_add = [];
-    text_del = [];
+    text_add = []
+    text_del = []
     cell_val = array_df[lin][col]
     tot_all = array_df[-1][-1]
     per = (float(cell_val) / tot_all) * 100
@@ -45,23 +46,23 @@ def configcell_text_and_colors(array_df, lin, col, oText, facecolors, posi, fz, 
     # last line  and/or last column
     if (col == (ccl - 1)) or (lin == (ccl - 1)):
         # tots and percents
-        if (cell_val != 0):
+        if cell_val != 0:
             if (col == ccl - 1) and (lin == ccl - 1):
                 tot_rig = 0
                 for i in range(array_df.shape[0] - 1):
                     tot_rig += array_df[i][i]
                 per_ok = (float(tot_rig) / cell_val) * 100
-            elif (col == ccl - 1):
+            elif col == ccl - 1:
                 tot_rig = array_df[lin][lin]
                 per_ok = (float(tot_rig) / cell_val) * 100
-            elif (lin == ccl - 1):
+            elif lin == ccl - 1:
                 tot_rig = array_df[col][col]
                 per_ok = (float(tot_rig) / cell_val) * 100
             per_err = 100 - per_ok
         else:
             per_ok = per_err = 0
 
-        per_ok_s = ['%.2f%%' % (per_ok), '100%'][per_ok == 100]
+        per_ok_s = ['%.2f%%' % per_ok, '100%'][per_ok == 100]
 
         # text to DEL
         text_del.append(oText)
@@ -69,14 +70,14 @@ def configcell_text_and_colors(array_df, lin, col, oText, facecolors, posi, fz, 
         # text to ADD
         font_prop = fm.FontProperties(weight='bold', size=fz)
         text_kwargs = dict(color='w', ha="center", va="center", gid='sum', fontproperties=font_prop)
-        lis_txt = ['%d' % (cell_val), per_ok_s, '%.2f%%' % (per_err)]
+        lis_txt = ['%d' % cell_val, per_ok_s, '%.2f%%' % per_err]
         lis_kwa = [text_kwargs]
-        dic = text_kwargs.copy();
-        dic['color'] = 'g';
-        lis_kwa.append(dic);
-        dic = text_kwargs.copy();
-        dic['color'] = 'r';
-        lis_kwa.append(dic);
+        dic = text_kwargs.copy()
+        dic['color'] = 'g'
+        lis_kwa.append(dic)
+        dic = text_kwargs.copy()
+        dic['color'] = 'r'
+        lis_kwa.append(dic)
         lis_pos = [(oText._x, oText._y - 0.3), (oText._x, oText._y), (oText._x, oText._y + 0.3)]
         for i in range(len(lis_txt)):
             newText = dict(x=lis_pos[i][0], y=lis_pos[i][1], text=lis_txt[i], kw=lis_kwa[i])
@@ -91,19 +92,19 @@ def configcell_text_and_colors(array_df, lin, col, oText, facecolors, posi, fz, 
         facecolors[posi] = carr
 
     else:
-        if (per > 0):
+        if per > 0:
             txt = '%s\n%.2f%%' % (cell_val, per)
         else:
-            if (show_null_values == 0):
+            if show_null_values == 0:
                 txt = ''
-            elif (show_null_values == 1):
+            elif show_null_values == 1:
                 txt = '0'
             else:
                 txt = '0\n0.0%'
         oText.set_text(txt)
 
         # main diagonal
-        if (col == lin):
+        if col == lin:
             # set color of the textin the diagonal to white
             oText.set_color('w')
             # set background color in the diagonal to blue
@@ -131,7 +132,7 @@ def insert_totals(df_cm):
 
 
 def pretty_plot_confusion_matrix(df_cm, annot=True, cmap="Oranges", fmt='.2f', fz=11,
-                                 lw=0.5, cbar=False, figsize=[8, 8], show_null_values=0, pred_val_axis='y'):
+                                 lw=0.5, cbar=False, figsize=None, show_null_values=0, pred_val_axis='y'):
     """
       print conf matrix with default layout (like matlab)
       params:
@@ -144,7 +145,9 @@ def pretty_plot_confusion_matrix(df_cm, annot=True, cmap="Oranges", fmt='.2f', f
                         'col' or 'x': show predicted values in columns (x axis) instead lines
                         'lin' or 'y': show predicted values in lines   (y axis)
     """
-    if (pred_val_axis in ('col', 'x')):
+    if figsize is None:
+        figsize = [8, 8]
+    if pred_val_axis in ('col', 'x'):
         xlbl = 'Predicted'
         ylbl = 'Actual'
     else:
@@ -180,13 +183,13 @@ def pretty_plot_confusion_matrix(df_cm, annot=True, cmap="Oranges", fmt='.2f', f
 
     # iter in text elements
     array_df = np.array(df_cm.to_records(index=False).tolist())
-    text_add = [];
-    text_del = [];
+    text_add = []
+    text_del = []
     posi = -1  # from left to right, bottom to top.
     for t in ax.collections[0].axes.texts:  # ax.texts:
         pos = np.array(t.get_position()) - [0.5, 0.5]
-        lin = int(pos[1]);
-        col = int(pos[0]);
+        lin = int(pos[1])
+        col = int(pos[0])
         posi += 1
         # print ('>>> pos: %s, posi: %s, val: %s, txt: %s' %(pos, posi, array_df[lin][col], t.get_text()))
 
@@ -212,22 +215,24 @@ def pretty_plot_confusion_matrix(df_cm, annot=True, cmap="Oranges", fmt='.2f', f
 
 
 def plot_confusion_matrix_from_data(y_test, predictions, columns=None, annot=True, cmap="Oranges",
-                                    fmt='.2f', fz=11, lw=0.5, cbar=False, figsize=[8, 8], show_null_values=0,
+                                    fmt='.2f', fz=11, lw=0.5, cbar=False, figsize=None, show_null_values=0,
                                     pred_val_axis='lin'):
     """
         plot confusion matrix function with y_test (actual values) and predictions (predic),
         whitout a confusion matrix yet
     """
+    if figsize is None:
+        figsize = [8, 8]
     from sklearn.metrics import confusion_matrix
     from pandas import DataFrame
 
     # data
-    if (not columns):
+    if not columns:
         # labels axis integer:
-        ##columns = range(1, len(np.unique(y_test))+1)
+        # # columns = range(1, len(np.unique(y_test))+1)
         # labels axis string:
         from string import ascii_uppercase
-        columns = ['class %s' % (i) for i in list(ascii_uppercase)[0:len(np.unique(y_test))]]
+        columns = ['class %s' % i for i in list(ascii_uppercase)[0:len(np.unique(y_test))]]
 
     confm = confusion_matrix(y_test, predictions)
     cmap = 'Oranges'
